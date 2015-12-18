d33f28af33d4a72d065af54e004875bb4b947b33
truenachtara <shybaconator@gmail.com>
2015-12-18 10:46:01 -0600

    Update counterWorker.js

--- counterWorker.js
+++ counterWorker.js
@@ -1,7 +1,9 @@
 var i = 0;
 
 function incrementMoney() {
-  this.onmessage = function(event)
+  this.onmessage = function(event) {
+     i = i - event.data
+  }
   i = i + 1;
   postMessage(i);
   setTimeout("incrementMoney()",1000);
