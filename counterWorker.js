19df6ee3eb1336456c19382b584d1bcee21ac33d
truenachtara <shybaconator@gmail.com>
2015-12-18 11:53:17 -0600

    Update counterWorker.js

--- counterWorker.js
+++ counterWorker.js
@@ -1,22 +1,24 @@
 var x = 0, dx = 1, current_action='add';
 var actions{
-'add' : function incrementMoney() { x += dx; },
-'subtract': function decrementMoney() { x -= dx; }
+'add' : function incrementMoney() {x += dx;},
+'subtract': function decrementMoney() {x -= dx;}
 };
 
 onmessage = function(evt) {
-   if (evt.data && evt.data.action && evt.data.action in actions)
-   {
-     current_action = evt.data.action;
-     if (evt.data.amount) dx = evt.data.amount;
+    if (evt.data && evt.data.action && evt.data.action in actions)
+    {
+      current_action = evt.data.action;
+      if (evt.data.amount) {
+      dx = evt.data.amount;
+      }
    }
 };
 
 function doAction()
 {
-   actions[current_action]();
-   postMessage(x);   
-   setTimeout(doAction,1000);
+    actions[current_action]();
+    postMessage(x);   
+    setTimeout(doAction,1000);
 }
 
 doAction();
